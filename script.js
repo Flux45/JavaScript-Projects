281f7374d9d5fd9247dd1e57d7145d643c50b181
Flux45 <116813328+Flux45@users.noreply.github.com>
2023-01-13 18:35:41 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-console.log('testing');
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+    // Pick a random Quote form apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    // Check if Author field is blank replace it with unknown.
+    if(!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+    //Check Quote Length to determine styling
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;
+}
+// Get Quotes From API
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+       const response = await fetch(apiUrl); 
+       apiQuotes = await response.json();
+       newQuote();
+    } catch (error) {
+        // Carch Error Here
+    }
+}
+
+//On Load
+getQuotes();
